9a925e607b3f8f50c6827403e39d3279f2a435c9
Ben Arent <ben@goteleport.com>
2021-06-21 14:33:59 -0700

    Update app.js

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.get("/", function (req, res) {
     ejs.render(
       `
     <p>Hello <b><%= username %></b>!</p>
-   <p>You are now logged in and have these roles.
+   <p>You are now logged in and have these roles..
    <ul>
      <% roles.forEach(role => { %>
        <li><%= role %></li>
